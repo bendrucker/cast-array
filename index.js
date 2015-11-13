3ce488b1204de8ab2a28e640eabed6d6456f748d
Jouni Kaplas <jouni.kaplas@iki.fi>
2015-11-13 09:57:21 +0200

    Add support for IE8 (if eg. used with browserify)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
 module.exports = function castArray (value) {
-  return Array.isArray(value) ? value : [value]
+  var isArray = Object.prototype.toString.call(value) === '[object Array]'
+  return isArray ? value : [value]
 }
